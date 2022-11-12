33b503ac287e6c17aee23437ec94ac3a92c7b301
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-11-12 20:07:08 +0300

    belge-kontraktı

--- digiathon-contracts/Belge.sol
+++ digiathon-contracts/Belge.sol
@@ -10,7 +10,7 @@ interface NoterKayit {
 contract Belge {
     struct BireyselBelge {
         uint256 zamanDamgasi;
-        address[] imzalayanlar;
+        address imzalayanNoter;
         bool imzaliMi;
     }
 
@@ -21,12 +21,12 @@ contract Belge {
         bool imzaliMi;
     }
 
-    uint256 antlasmaSayisi;
-    uint256 belgeSayisi;
-
     mapping(bytes => BireyselBelge) public bireyselBelgeler;
     mapping(bytes => Antlasma) public antlasmalar;
-    mapping(address => uint256[]) public adresinBelgeleri;
+    mapping(address => bytes[]) public adresinBelgeleri;
+    mapping(address => mapping(bytes => bool)) public imzalananBelgeler;
+    mapping(address => mapping(bytes => bool)) public imzalanabilirBelgeler;
+    mapping(bytes => uint256) public hangiBelgeTuru; //0 = tanimsiz, 1 = belge, 2 = antlasma
 
     NoterKayit public noterKayit;
 
@@ -35,46 +35,71 @@ contract Belge {
     }
 
     function bireyselBelgeOlustur(bytes calldata _belgeHashi) external {
-        address[] memory _imzalayanlar;
-        bireyselBelgeler[_belgeHashi] = BireyselBelge(
-            block.timestamp,
-            _imzalayanlar,
-            false
-        );
+        BireyselBelge memory belge;
+        belge.zamanDamgasi = block.timestamp;
+        bireyselBelgeler[_belgeHashi] = belge;
+        hangiBelgeTuru[_belgeHashi] = 1;
     }
 
     function antlasmaOlustur(
         bytes calldata _belgeHashi,
         address[] calldata _taraflar
     ) external {
-        address[] memory _imzalayanlar;
-        antlasmalar[_belgeHashi] = Antlasma(
-            block.timestamp,
-            _imzalayanlar,
-            _taraflar,
-            false
-        );
+        Antlasma memory _antlasma;
+        _antlasma.taraflar = _taraflar;
+        _antlasma.zamanDamgasi = block.timestamp;
+        antlasmalar[_belgeHashi] = _antlasma;
+        for (uint256 i = 0; i < _taraflar.length; i++) {
+            imzalanabilirBelgeler[_taraflar[i]][_belgeHashi] = true;
+        }
+        hangiBelgeTuru[_belgeHashi] = 2;
     }
 
-    function noterOlarakImzala(
-        bytes calldata _belgeHashi,
-        bool _antlasmaVeyaBelge
-    ) external {
+    function noterOlarakImzala(bytes calldata _belgeHashi) external {
         require(noterKayit.noterMi(msg.sender), "Bu adres noter degildir");
-        if (_antlasmaVeyaBelge) {
+        require(hangiBelgeTuru[_belgeHashi] != 0, "Gecersiz belge");
+        if (hangiBelgeTuru[_belgeHashi] == 2) {
             require(
                 antlasmalar[_belgeHashi].imzaliMi,
                 "Bu belge zaten noter tarafindan imzalanmistir"
             );
             antlasmalar[_belgeHashi].imzalayanlar.push(msg.sender);
             antlasmalar[_belgeHashi].imzaliMi = true;
-        } else {
+            imzalananBelgeler[msg.sender][_belgeHashi] = true;
+        } else if (hangiBelgeTuru[_belgeHashi] == 1) {
             require(
                 bireyselBelgeler[_belgeHashi].imzaliMi,
                 "Bu belge zaten noter tarafindan imzalanmistir"
             );
-            bireyselBelgeler[_belgeHashi].imzalayanlar.push(msg.sender);
+            bireyselBelgeler[_belgeHashi].imzalayanNoter = msg.sender;
             bireyselBelgeler[_belgeHashi].imzaliMi = true;
+            imzalananBelgeler[msg.sender][_belgeHashi] = true;
         }
+    }
+
+    function bireyOlarakImzala(bytes calldata _belgeHashi) external {
+        require(
+            hangiBelgeTuru[_belgeHashi] == 2,
+            "Bu belge yalnizca noter tarafindan imzalanabilir ya da gecersiz belge"
+        );
+        require(
+            imzalanabilirBelgeler[msg.sender][_belgeHashi],
+            "Bu belgeyi imzalayabilmek icin yetkiniz yoktur"
+        );
+        require(
+            imzalananBelgeler[msg.sender][_belgeHashi] == false,
+            "Bu belgeyi coktan imzaladiniz"
+        );
+
+        antlasmalar[_belgeHashi].imzalayanlar.push(msg.sender);
+        imzalananBelgeler[msg.sender][_belgeHashi] = true;
+    }
+
+    function antlasmayiKimImzaladi(bytes calldata _belgeHashi)
+        external
+        view
+        returns (address[] memory)
+    {
+        return antlasmalar[_belgeHashi].imzalayanlar;
     }
 }
