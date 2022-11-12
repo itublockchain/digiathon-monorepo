29abe007b4c768abb5722080a120b28abc469c3c
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-11-12 14:15:43 +0300

    login-page-wallet-update

--- digiathon-frontend/pages/login.tsx
+++ digiathon-frontend/pages/login.tsx
@@ -1,9 +1,13 @@
 import { clsnm } from '@ethylene/utils';
 import { NextPage } from 'next';
 import trLogin from '../assets/tr-login.png';
+import { AiOutlineWallet, AiOutlineLock } from 'react-icons/ai';
 import styles from 'styles/login.module.scss';
+import { useState } from 'react';
 
 const Login: NextPage = () => {
+  const [loginChoose, setLoginChoose] = useState('password');
+
   return (
     <div
       className={clsnm(
@@ -17,51 +21,77 @@ const Login: NextPage = () => {
         </header>
         <main>
           <section>
-            <img src={trLogin.src} alt="" width="165" height="40" />
+            <h3>Giriş Yapılacak Adres www.turkiye.gov.tr</h3>
+            <h3>Giriş Yapılacak Adres www.turkiye.gov.tr</h3>
+            <img
+              className="float-right"
+              src={trLogin.src}
+              alt=""
+              width="165"
+              height="40"
+            />
           </section>
           <nav className={clsnm(styles.methodSelector)}>
             <div className={clsnm(styles.tabChoser)}>
               <nav className={clsnm(styles.tabList)}>
                 <div className={clsnm(styles.tab)}>
-                  <span>E-Devlet Şifresi</span>
+                  <a onClick={() => setLoginChoose('password')}>
+                    <AiOutlineLock size="30" />
+                    <span>E-Devlet Şifresi</span>
+                  </a>
                 </div>
                 <div className={clsnm(styles.tab)}>
-                  <span>Mobil İmza</span>
-                </div>
-                <div className={clsnm(styles.tab)}>
-                  <span>e-İmza</span>
-                </div>
-                <div className={clsnm(styles.tab)}>
-                  <span>T.C. Kimlik Kartı</span>
-                </div>
-                <div className={clsnm(styles.tab)}>
-                  <span>İnternet Bankacılığı</span>
+                  <a onClick={() => setLoginChoose('wallet')}>
+                    <AiOutlineWallet size="30" />
+                    <span>Mobil Cüzdan</span>
+                  </a>
                 </div>
               </nav>
             </div>
-            <section className={clsnm(styles.pageContent)}>
-              <div className={clsnm(styles.richText)}>
-                "T.C. Kimlik Numaranızı ve e-Devlet Şifrenizi kullanarak
-                kimliğiniz doğrulandıktan sonra işleminize kaldığınız yerden
-                devam edebilirsiniz."
+            {loginChoose == 'password' && (
+              <section className={clsnm(styles.pageContent)}>
+                <div className={clsnm(styles.richText)}>
+                  T.C. Kimlik Numaranızı ve e-Devlet Şifrenizi kullanarak
+                  kimliğiniz doğrulandıktan sonra işleminize kaldığınız yerden
+                  devam edebilirsiniz.
+                </div>
+                <div className={clsnm(styles.fieldSet)}>
+                  <form className={clsnm(styles.inputForm)}>
+                    <div className={clsnm(styles.formRow)}>
+                      <label className={clsnm(styles.rowLabel)}>
+                        T.C. Kimlik No
+                      </label>
+                      <input type="text"></input>
+                    </div>
+                    <div className={clsnm(styles.formRow)}>
+                      <label className={clsnm(styles.rowLabel)}>
+                        E-Devlet Şifresi
+                      </label>
+                      <input type="password"></input>
+                      <div className={clsnm(styles.fieldInfo, styles.richText)}>
+                        * e-Devlet şifrenizi unutmanız durumunda doğruladığınız
+                        cep telefonunuzdan yenileme işlemi yapabilirsiniz.
+                      </div>
+                    </div>
+                    <div className={clsnm(styles.formSubmit)}>
+                      <button className={clsnm(styles.backButton)}>
+                        {'İptal'}
+                      </button>
+                      <button className={clsnm(styles.submitButton)}>
+                        Giriş Yap
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </section>
+            )}
+            {loginChoose == 'wallet' && (
+              <div className="h-60 flex justify-center items-center">
+                <button className={clsnm(' w-auto h-5', styles.submitButton)}>
+                  Mobil Cüzdanımı Bağla
+                </button>
               </div>
-              <div className={clsnm(styles.fieldSet)}>
-                <form className={clsnm(styles.inputForm)} method="post">
-                  <div className={clsnm(styles.formRow)}>
-                    <label className={clsnm(styles.rowLabel)}>
-                      T.C. Kimlik No
-                    </label>
-                    <input></input>
-                  </div>
-                  <div className={clsnm(styles.formRow)}>
-                    <label className={clsnm(styles.rowLabel)}>
-                      T.C. Kimlik No
-                    </label>
-                    <input></input>
-                  </div>
-                </form>
-              </div>
-            </section>
+            )}
           </nav>
         </main>
       </div>
