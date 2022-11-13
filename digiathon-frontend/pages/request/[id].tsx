fb1592106819b5358f45c6004d093bc9dde96f9a
asgarovf <fasgarov2002@gmail.com>
2022-11-13 08:05:03 +0300

    feat: showing document hash

--- digiathon-frontend/pages/request/[id].tsx
+++ digiathon-frontend/pages/request/[id].tsx
@@ -174,11 +174,13 @@ const Request = () => {
                 width: '300px',
                 height: '300px',
                 position: 'fixed',
-                left: '150x',
-                bottom: '25%',
+                left: '50%',
+                transform: 'translate(-40%)',
+                bottom: '12px',
               }}
               src={qrData.image}
             />
+            <span className="mt-2 block">{request?.document?.hash ?? ''}</span>
           </div>
         </Modal>
       )}
